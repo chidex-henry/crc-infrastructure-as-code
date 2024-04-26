8c615e7f3621ae96faa4942d8874af11dd3f8e65
Chidi Ukaegbu <chidex.ukaegbu4real@gmail.com>
2024-04-26 09:46:17 -0500

    provider config

---
+++ variables.tf
@@ -0,0 +1,11 @@
+#environment variables 
+variable "project_name" {
+  description = "project name"
+  type = string
+}
+
+variable "environment" {
+  description = "environment"
+  type = string
+}
+
